12d9ada7ff24625a662211faac9b72b5b88aacc5
DzLyz <86110711+itsdzl@users.noreply.github.com>
2022-01-08 10:52:12 +0700

    Update vplay.py

--- Music/Plugins/vplay.py
+++ Music/Plugins/vplay.py
@@ -15,7 +15,6 @@ from pytgcalls.types.input_stream.quality import (
 from youtubesearchpython import VideosSearch
 
 from Music import BOT_NAME, BOT_USERNAME, app
-from Music.config import SUPPORT_GROUP, UPDATES_CHANNEL
 from Music.MusicUtilities.helpers.filters import command
 from Music.MusicUtilities.helpers.logger import LOG_CHAT
 from Music.MusicUtilities.tgcallsrun import ASS_ACC
