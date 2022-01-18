9caeb762a266942712057478ad048ca0a6703118
stuntrider123 <55699161+stuntrider123@users.noreply.github.com>
2022-01-18 12:53:43 +0700

    Update inline.py

--- Music/MusicUtilities/helpers/inline.py
+++ Music/MusicUtilities/helpers/inline.py
@@ -9,8 +9,11 @@ from pyrogram.types import (
 def play_markup(videoid, user_id):
     buttons= [
             [
-            InlineKeyboardButton(text="Zin", url="https://t.me/Zackin"),
-        ],
+                InlineKeyboardButton(text="▷", callback_data=f'resumevc2'),
+                InlineKeyboardButton(text="II", callback_data=f'pausevc2'),
+                InlineKeyboardButton(text="‣‣I", callback_data=f'skipvc2'),
+                InlineKeyboardButton(text="▢", callback_data=f'stopvc2')
+            ],
         [      
                 InlineKeyboardButton(text="•ᴍᴇɴᴜ•", callback_data=f"other {videoid}|{user_id}")
         ],
